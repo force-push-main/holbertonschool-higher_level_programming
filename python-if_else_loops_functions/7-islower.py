f9e4866a3e055a7362a8b892ae75e07f7e896268
force-push-main <10535@holbertonstudents.com>
2025-05-06 14:28:56 +1000

    fixed code for islower

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
-def isLower(c):
-    if ("abcdefghijklmnopqrstuvwxyz".find(c) < 0):
+def islower(c):
+    if (ord(c) > 96 and ord(c) < 123):
         return True
     else:
         return False
