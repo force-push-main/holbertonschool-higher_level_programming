7bfe689374b194f462646a4870ffc80a22bf25e8
force-push-main <10535@holbertonstudents.com>
2025-05-06 14:04:14 +1000

    stoopid format function

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -2,4 +2,4 @@
 
 for i in "abcdefghijklmnopqrstuvwxyz":
     if (i != 'q' and i != 'e'):
-        print(f"{i}", end="")
+        print("{}".format(i), end="")
---
+++ python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def isLower(c):
+    if ("abcdefghijklmnopqrstuvwxyz".find(c) < 0):
+        return True
+    else:
+        return False
