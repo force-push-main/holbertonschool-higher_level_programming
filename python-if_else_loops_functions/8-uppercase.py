3dbc04a0c33f9072b342114452ab15e940c66921
force-push-main <10535@holbertonstudents.com>
2025-05-06 15:08:43 +1000

    added damn format thing

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -4,4 +4,4 @@ def uppercase(str):
     for i in range(len(str)):
         if (ord(str[i]) > 96):
             str = str[:i] + chr(ord(str[i]) - 32) + str[i + 1:]
-    print(str)
+    print("{}".format(str))
