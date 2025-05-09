7a702578a6d3bfde96605390aafbbbb3a435be19
force-push-main <10535@holbertonstudents.com>
2025-05-09 14:36:14 +1000

    added code for task 6

---
+++ python-import_modules/100-my_calculator.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+from calculator_1.py import add, sub, mul, div
+import sys
+
+
+def main():
+    argv = sys.argv[1:]
+    calc_fncs = {
+        "+": add,
+        "-": sub,
+        "*": mul,
+        "/": div
+    }
+
+    if (len(argv) < 3):
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+        if argv[1] in calc_fncs:
+            print(calc_fncs[argv[1]](int(argv[0]), int(argv[2])))
+        else:
+            print("Unknown operator. Available operators: +, -, * and /")
+            exit(1)
+
+
+if __name__ == "__main__":
+    main()
---
+++ python-import_modules/new_file.py
@@ -0,0 +1,3 @@
+#!/usr/bin/sudo /usr/bin/python3
+
+print("it works")
