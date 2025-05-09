dc5a1a3a8af5cc7e6d73da0c037c7b67e2367161
force-push-main <10535@holbertonstudents.com>
2025-05-09 14:45:44 +1000

    different import method

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,25 +1,26 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
+import calculator1
 import sys
 
 
 def main():
     argv = sys.argv[1:]
     calc_fncs = {
-        "+": add,
-        "-": sub,
-        "*": mul,
-        "/": div
+        "+": calculator1.add,
+        "-": calculator1.sub,
+        "*": calculator1.mul,
+        "/": calculator1.div
     }
 
     if (len(argv) < 3):
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
         if argv[1] in calc_fncs:
-            print(calc_fncs[argv[1]](int(argv[0]), int(argv[2])))
+            result = calc_fncs[argv[1]](int(argv[0]), int(argv[2]))
         else:
             print("Unknown operator. Available operators: +, -, * and /")
             exit(1)
+        print("{} + {} = {}".format(argv[0], argv[2], result))
 
 
 if __name__ == "__main__":
