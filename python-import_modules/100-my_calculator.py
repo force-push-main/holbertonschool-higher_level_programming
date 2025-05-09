570626bbc1ec8b5cde79ef7800c8a93d2a4e5fba
force-push-main <10535@holbertonstudents.com>
2025-05-09 14:50:31 +1000

    more specific import of sys

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,26 +1,25 @@
 #!/usr/bin/python3
-import calculator1
-import sys
-
+from calculator_1 import add, sub, mul, div
+from sys import argv
 
 def main():
-    argv = sys.argv[1:]
+    args = argv[1:]
     calc_fncs = {
-        "+": calculator1.add,
-        "-": calculator1.sub,
-        "*": calculator1.mul,
-        "/": calculator1.div
+        "+": add,
+        "-": sub,
+        "*": mul,
+        "/": div
     }
 
-    if (len(argv) < 3):
+    if (len(args) < 3):
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-        if argv[1] in calc_fncs:
-            result = calc_fncs[argv[1]](int(argv[0]), int(argv[2]))
+        if args[1] in calc_fncs:
+            result = calc_fncs[args[1]](int(args[0]), int(args[2]))
         else:
             print("Unknown operator. Available operators: +, -, * and /")
             exit(1)
-        print("{} + {} = {}".format(argv[0], argv[2], result))
+        print("{} + {} = {}".format(args[0], args[2], result))
 
 
 if __name__ == "__main__":
