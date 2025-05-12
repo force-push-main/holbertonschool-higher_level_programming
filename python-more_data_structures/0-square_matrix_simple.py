84d87eb555619cc4a82a25c3c581a796aba465f9
force-push-main <10535@holbertonstudents.com>
2025-05-12 13:04:11 +1000

    changed wrong matrix

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -4,5 +4,5 @@ def square_matrix_simple(matrix=[]):
     squared = matrix.copy()
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
-            matrix[i][j] = matrix[i][j]**2
+            squared[i][j] = matrix[i][j]**2
     return squared
