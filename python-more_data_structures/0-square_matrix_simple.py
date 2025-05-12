6ab74db77bb57f32e128ffd0309cff7982466af2
force-push-main <10535@holbertonstudents.com>
2025-05-12 13:39:46 +1000

    added code for task 0

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-    squared = matrix.copy()
-    for i in range(len(squared)):
-        for j in range(len(squared[i])):
-            squared[i][j]**2
-    return squared
+    return [num ** 2 for row in matrix for num in row]
