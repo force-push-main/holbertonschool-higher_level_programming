eda27582bcbee25868a60896458680bf2934a679
force-push-main <10535@holbertonstudents.com>
2025-05-12 12:13:56 +1000

    added code for task 7

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -9,7 +9,7 @@ def add_tuple(tuple_a=(), tuple_b=()):
     elif b_len == 0:
         return tuple_a
     elif a_len == 1 and b_len == 1:
-        return (tuple_a[0] + tuple_b[0], )
+        return (tuple_a[0] + tuple_b[0], 0)
     elif a_len == 1:
         return (tuple_a[0] + tuple_b[0], tuple_b[1])
     elif b_len == 1:
