15e1abac02315769169e0b8cc8dcd0d79e57f13a
force-push-main <10535@holbertonstudents.com>
2025-05-13 14:30:18 +1000

    added code for task 4

---
+++ python-exceptions/4-list_division.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+def list_division(my_list_1, my_list_2, list_length):
+    new_list = []
+    for i in range(list_length):
+        try:
+            to_add = my_list_1[i] / my_list_2[i]
+        except IndexError:
+            to_add = 0
+            print("out of range")
+            return new_list
+        except ZeroDivisionError:
+            to_add = 0
+            print("division by 0")
+        except TypeError:
+            to_add = 0
+            print("wrong type")
+        finally:
+            new_list.append(to_add)
+    return new_list
