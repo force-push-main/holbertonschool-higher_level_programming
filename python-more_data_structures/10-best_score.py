bf813f0b0cb53e3b1ea3499eaba9e988f13ad480
force-push-main <10535@holbertonstudents.com>
2025-05-13 11:07:15 +1000

    needs key not value

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -7,4 +7,5 @@ def best_score(a_dictionary):
     for key in a_dictionary:
         if a_dictionary[key] > max_score:
             max_score = a_dictionary[key]
-    return max_score
+            name = key
+    return name
