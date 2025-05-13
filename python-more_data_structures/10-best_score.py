24c74b2281b5b7e6620af5a1123fc68053e6f9fe
force-push-main <10535@holbertonstudents.com>
2025-05-13 11:03:42 +1000

    added code for task 10

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def best_score(a_dictionary):
+    if not a_dictionary:
+        return None
+    max_score = 0
+    for key in a_dictionary:
+        if a_dictionary[key] > max_score:
+            max_score = a_dictionary[key]
+    return max_score
