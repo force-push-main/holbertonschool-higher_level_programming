f9e19281a34aaea3e85be072322d6be3cdc16dbf
force-push-main <10535@holbertonstudents.com>
2025-05-13 14:32:14 +1000

    have to append before returning on index error

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -6,7 +6,7 @@ def list_division(my_list_1, my_list_2, list_length):
         try:
             to_add = my_list_1[i] / my_list_2[i]
         except IndexError:
-            to_add = 0
+            new_list.append(0)
             print("out of range")
             return new_list
         except ZeroDivisionError:
