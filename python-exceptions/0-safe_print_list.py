f743b9cba550255ce717d0eabd01759078d62e9a
force-push-main <10535@holbertonstudents.com>
2025-05-13 13:51:15 +1000

    added condition to except

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -6,7 +6,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print("{}".format(my_list[i]), end="")
             chars_printed = chars_printed + 1
-        except:
+        except IndexError:
             print("")
             return chars_printed
     print("")
