26ccc1d28e18356c9f385ae5557ca51abda063f0
force-push-main <10535@holbertonstudents.com>
2025-05-14 13:25:42 +1000

    added code for task 1

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
 # 2-matrix_divided.py
 
+
 def div_el(el, div):
+    """helper function to divide els in array"""
     if not isinstance(el, int) and not isinstance(el, float):
         raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
     else:
         return round(el / div, 2)
 
+
 def matrix_divided(matrix, div):
+    """divides all elements in matrix by div and returns new matrix"""
     if not isinstance(div, int):
         raise TypeError("div must be a number")
     if div == 0:
@@ -17,10 +21,3 @@ def matrix_divided(matrix, div):
             raise TypeError("Each row of the matrix must have the same size")
     new_matrix = [list(map(lambda el: div_el(el, div), row)) for row in matrix]
     return new_matrix
-
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6]
-]
-print(matrix_divided(matrix, 3))
-print(matrix)
