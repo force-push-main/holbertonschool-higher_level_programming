4a6a1374635ff5906b039addf721a35cda06f2ea
force-push-main <10535@holbertonstudents.com>
2025-05-15 11:20:05 +1000

    added code for task 5

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""function prints text with indents"""
+
+
+def text_indentation(text):
+    """function reformats text based on spec chars"""
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    spec_chars = ['.', '?', ':']
+    printed_newline = False
+    for c in text:
+        if printed_newline:
+            printed_newline = False
+            continue
+        if c in spec_chars:
+            print(f"{c}\n")
+            printed_newline = True
+        else:
+            print(c, end="")
