46edf7310c73624d60efff4029769a8b35a3c940
force-push-main <10535@holbertonstudents.com>
2025-05-15 10:46:17 +1000

    added code for task 4

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""function prints square"""
+
+def print_square(size):
+    """function that prints square of size*size"""
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    for i in range(size):
+        print(f'#' * size)
