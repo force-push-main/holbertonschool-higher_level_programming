6c49a3962b42c30653cd442d25306ebe776dd138
force-push-main <10535@holbertonstudents.com>
2025-05-15 11:37:04 +1000

    added code for task 5

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -6,14 +6,14 @@ def text_indentation(text):
     """function reformats text based on spec chars"""
     if not isinstance(text, str):
         raise TypeError("text must be a string")
-    spec_chars = ['.', '?', ':']
+    spec_chars = ".?:"
+    alpha_chars = "abcdefghijklmnopqrstuvwxyz"
     printed_newline = False
     for c in text:
-        if printed_newline and (c == ' ' or c == '  '):
+        if printed_newline and c.lower() in alpha_chars:
             printed_newline = False
+        elif printed_newline:
             continue
-        else:
-            printed_newline = False
         if c in spec_chars:
             print(f"{c}\n")
             printed_newline = True
