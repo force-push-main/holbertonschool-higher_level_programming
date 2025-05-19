9f8ceeef1e3f1f523ee0bdbdd05624791aa45419
force-push-main <10535@holbertonstudents.com>
2025-05-19 12:21:51 +1000

    added code for task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+"""this function declares a class for a square object"""
+
+
+class Square:
+    """this is a square object. It is empty"""
+    def __init__(self, size):
+        self.__size = size
