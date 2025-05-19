63419dd3a8a641eca5450167e56e17589150c5de
force-push-main <10535@holbertonstudents.com>
2025-05-19 15:00:44 +1000

    is this what they want?

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -41,7 +41,7 @@ class Square:
         if self.size == 0:
             print("")
         else:
-            print(' ' * self.position[1])
+            print('' * self.position[1])
             for i in range(self.size):
                 print(' ' * self.position[0], end='')
                 print('#' * self.size)
