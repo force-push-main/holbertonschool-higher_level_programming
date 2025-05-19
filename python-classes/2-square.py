bdbfecb4a7b10f725860bd4aa4782b30786d2181
force-push-main <10535@holbertonstudents.com>
2025-05-19 13:35:49 +1000

    added code for task 1 and 2

---
+++ python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""this function declares a class for a square object"""
+
+
+class Square:
+    """this is a square object. It is empty"""
+    def __init__(self, size=0):
+            self.__size = None
+            if not isinstance(size, int):
+                raise TypeError("size must be an integer")
+            elif size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = size
