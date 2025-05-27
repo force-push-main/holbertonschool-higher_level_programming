e49b81df067f0882779928615facea02b87bbc83
force-push-main <10535@holbertonstudents.com>
2025-05-27 13:17:30 +1000

    added code for task 7

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""empty class called base geometry"""
+
+
+class BaseGeometry:
+    """class with public instance method"""
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            msg = f"{name} must be an integer"
+            raise TypeError(msg)
+        if value <= 0:
+            msg = f"{name} must be greater than 0"
+            raise ValueError(msg)
