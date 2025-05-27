a823ef23069e7b24a7285ab788a840a9860d5b09
force-push-main <10535@holbertonstudents.com>
2025-05-27 14:16:04 +1000

    added code for task 10

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""creates subclass of rectangle"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """square class inherits rectangle"""
+    def __init__(self, size):
+        super().__init__(size, size)
+        self.__size = size
