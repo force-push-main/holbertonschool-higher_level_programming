abef5349b659f704fde0018bc18b711e47c753e9
force-push-main <10535@holbertonstudents.com>
2025-05-27 14:19:08 +1000

    rearranged validation

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -8,6 +8,6 @@ Rectangle = __import__('9-rectangle').Rectangle
 class Square(Rectangle):
     """square class inherits rectangle"""
     def __init__(self, size):
+        super().integer_validator("size", size)
         super().__init__(size, size)
-        self.integer_validator("size", size)
         self.__size = size
