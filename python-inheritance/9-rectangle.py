8a2c737a7ba37fa0a70464049273bc7d2d761ac4
force-push-main <10535@holbertonstudents.com>
2025-05-27 14:04:57 +1000

    added code for task 9

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -12,3 +12,9 @@ class Rectangle(BaseGeometry):
         self.__width = width
         self.integer_validator('height', height)
         self.__height = height
+
+    def area(self):
+        return self.__height * self.__width
+
+    def __str__(self):
+        return f"[Rectangle] {self.__width}/{self.__height}"
