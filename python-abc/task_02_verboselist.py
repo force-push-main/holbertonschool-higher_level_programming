89b6e767ebadf7475e117c80df6574c8bd359017
force-push-main <10535@holbertonstudents.com>
2025-05-28 12:48:35 +1000

    added code for task 2

---
+++ python-abc/task_02_verboselist.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""extended list class"""
+
+
+class VerboseList(list):
+    """class extends list class"""
+
+    def append(self, el):
+        super().append(el)
+        print(f"Added [{el}] to the list.")
+
+    def extend(self, iterable):
+        super().extend(iterable)
+        print(f"Extended the list with [{len(iterable)}] items")
+
+    def remove(self, el):
+        super().remove(el)
+        print(f"Removed [{el}] from the list.")
+
+    def pop(self, index=-1):
+        print(f"Popped [{self[index]}] from the list.")
+        super().pop(index)
