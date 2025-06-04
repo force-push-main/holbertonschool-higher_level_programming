3edb74638ec141e0cf48be2b1aca6d263a55b242
force-push-main <10535@holbertonstudents.com>
2025-06-04 11:53:27 +1000

    added code for task 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,18 +1,14 @@
 #!/usr/bin/python3
 """function adds args to list then writes to file"""
 import os
+import sys
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
-
-def add_to_list(*argv):
-    """add_to_list function"""
-
-    filename = "add_item.json"
-    if not os.path.exists('add_item.json'):
-        save_to_json_file([], filename)
-    data = load_from_json_file(filename)
-    for arg in argv:
-        data.append(arg)
-
-    save_to_json_file(data, filename)
+filename = "add_item.json"
+if not os.path.exists("add_item.json"):
+    save_to_json_file([], filename)
+data = load_from_json_file(filename)
+for arg in sys.argv[1:]:
+    data.append(arg)
+save_to_json_file(data, filename)
