64d67476d2cde0767c315599d0996c5f80239a3f
force-push-main <10535@holbertonstudents.com>
2025-06-04 10:59:28 +1000

    needs pointer

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -6,6 +6,6 @@ import json
 def load_from_json_file(filename):
     """load_from_json_file function"""
 
-    with open(filename, "r"):
-        data = json.load(filename)
+    with open(filename) as file:
+        data = json.load(file)
     return data
