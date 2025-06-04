e34442fea585c7e1670ac86f82944766d523113c
force-push-main <10535@holbertonstudents.com>
2025-06-04 14:59:18 +1000

    added code for task 11

---
+++ python-input_output/11-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""class that returns dict of self"""
+
+
+class Student():
+    """student class"""
+
+    def __init__(self, first_name, last_name, age):
+        self.age = age
+        self.last_name = last_name
+        self.first_name = first_name
+
+    def to_json(self, attrs=None):
+        if isinstance(attrs, list):
+            new_dict = {key: value for key,
+                        value in self.__dict__.items() if key in attrs}
+            return new_dict
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        for key in json:
+            self.key = json[key]
