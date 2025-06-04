063740006c62feae901de7af567e552579a86f2e
force-push-main <10535@holbertonstudents.com>
2025-06-04 15:10:43 +1000

    added code for task 11

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -6,9 +6,9 @@ class Student():
     """student class"""
 
     def __init__(self, first_name, last_name, age):
-        self.age = age
-        self.last_name = last_name
         self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
     def to_json(self, attrs=None):
         if isinstance(attrs, list):
@@ -18,5 +18,7 @@ class Student():
         return self.__dict__
 
     def reload_from_json(self, json):
+        class_dict = self.__dict__
         for key in json:
-            self.key = json[key]
+            print(class_dict[key])
+            class_dict[key] = json[key]
