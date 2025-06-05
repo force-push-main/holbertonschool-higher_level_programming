69594da2c425dc148946cb9cb1ac5e8b95526892
force-push-main <10535@holbertonstudents.com>
2025-06-05 12:03:44 +1000

    if its indents i'll scream

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -5,14 +5,16 @@ import json
 
 
 def convert_csv_to_json(filename):
-    cereal_json = []
     try:
         with open(filename, "r") as csv_file:
             data = list(csv.DictReader(csv_file))
-        for row in data:
-            cereal_json.append(row)
-        with open('data.json', "a") as json_file:
-            json.dump(cereal_json, json_file)
+
+        with open('data.json', "w") as json_file:
+            json.dump(data, json_file, indent=4)
         return True
     except:
         return False
+
+csv_file = "data.csv"
+convert_csv_to_json(csv_file)
+print(f"Data from {csv_file} has been converted to data.json")
