0c8ed62fe7dba07527b05bfd4401835c3ce7c9f4
force-push-main <10535@holbertonstudents.com>
2025-06-05 12:05:30 +1000

    removed tests

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -14,7 +14,3 @@ def convert_csv_to_json(filename):
         return True
     except:
         return False
-
-csv_file = "data.csv"
-convert_csv_to_json(csv_file)
-print(f"Data from {csv_file} has been converted to data.json")
