bf9da3fdad6273cc592a36bf099fc5407d1c3b61
force-push-main <10535@holbertonstudents.com>
2025-06-11 16:05:14 +1000

    changed status code

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -23,7 +23,7 @@ def api_status():
 @app.route('/users/<username>')
 def get_user(username):
     if username not in users:
-        return {"error": "User not found"}
+        return {"error": "User not found"}, 400
     return users[username]
 
 
