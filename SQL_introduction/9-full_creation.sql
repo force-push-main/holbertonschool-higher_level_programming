15020921cc3f2f72303e3e5a1155e191f899c8f7
force-push-main <10535@holbertonstudents.com>
2025-06-24 13:11:52 +1000

    typo

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,5 +1,5 @@
 -- creates a table with multiple rows
-CREATE TABLE if not EXISTS second_table (id int, name varchar(256), score int);
+CREATE TABLE IF NOT EXISTS second_table (id int, name varchar(256), score int);
 INSERT INTO second_table (id, name, int)
 VALUES 
 (1, 'John', 10), (2, 'Alex', 3), (3, 'Bob', 14), (4, "George", 8);
