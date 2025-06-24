ff7ce9254abd4e3c6a97692a94cc5d40cb1bc01d
force-push-main <10535@holbertonstudents.com>
2025-06-24 16:35:07 +1000

    backticks mean something

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,6 @@
 -- use join to find cities by state id
 SELECT cities.id, cities.name, states.name
-FROM cities
+FROM `cities`
 INNER JOIN cities ON states.id = cities.state_id
 WHERE states.name = 'California'
 ORDER BY cities.id ASC
