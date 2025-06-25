88ba072e1d9e428f58165908bfe511b3ff7d69e8
force-push-main <10535@holbertonstudents.com>
2025-06-25 13:44:52 +1000

    uppercase commands

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,5 @@
 -- number of shows related to genre
-SELECT tv_genres.name AS genres, COUNT(*) as number_of_shows
+SELECT tv_genres.name AS genres, COUNT(*) AS number_of_shows
 FROM tv_genres
 INNER JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
