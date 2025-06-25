7dbe5a05c9969f221b85a6ddd89b4efb7113ec64
force-push-main <10535@holbertonstudents.com>
2025-06-25 13:43:49 +1000

    added code for task 13

---
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,6 @@
+-- number of shows related to genre
+SELECT tv_genres.name AS genres, COUNT(*) as number_of_shows
+FROM tv_genres
+INNER JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
+ORDER BY number_of_shows DESC;
