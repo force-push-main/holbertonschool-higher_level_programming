d5af49cad6d2ded006f04f2f2813092173a7ac1a
force-push-main <10535@holbertonstudents.com>
2025-06-25 12:32:15 +1000

    left join

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,6 +1,6 @@
 -- list all shows in hbtn_0d_tvshows
-SELECT tv_shows.title, IFNULL(tv_show_genres.genre_id, NULL)
+SELECT tv_shows.title, IFNULL(tv_show_genres.genre_id, NULL) as genre_id
 from tv_shows
-INNER JOIN tv_show_genres 
+LEFT JOIN tv_show_genres 
 ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title, tv_show_genres.show_id;
