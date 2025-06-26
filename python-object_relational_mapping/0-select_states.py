f1476504227737bbac403815177da12f6155da71
force-push-main <10535@holbertonstudents.com>
2025-06-26 12:17:56 +1000

    pycodestyle

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -7,10 +7,13 @@ if __name__ == '__main__':
     import MySQLdb
     import sys
 
-
-    db=MySQLdb.connect(user=sys.argv[1], password=sys.argv[2], database=sys.argv[3])
+    db = MySQLdb.connect(
+        user=sys.argv[1], password=sys.argv[2], database=sys.argv[3]
+        )
     c = db.cursor()
-    c.execute("""SELECT states.id, states.name FROM states ORDER BY states.id ASC""")
+    c.execute("""SELECT states.id, states.name
+              FROM states
+              ORDER BY states.id ASC""")
     rows = c.fetchall()
     for row in rows:
         print(row)
