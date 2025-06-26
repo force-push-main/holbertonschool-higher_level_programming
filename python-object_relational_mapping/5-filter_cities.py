4ac55fcb904fd65a5c0abc309d572a81a0e4debe
force-push-main <10535@holbertonstudents.com>
2025-06-26 14:38:33 +1000

    added code for task 5

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""script that returns cities from state"""
+
+
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+
+    db = MySQLdb.connect(
+        user=sys.argv[1],
+        password=sys.argv[2],
+        database=sys.argv[3]
+        )
+    c = db.cursor()
+
+    c.execute(
+        """SELECT cities.name
+        FROM cities
+        INNER JOIN states
+        ON cities.state_id = states.id
+        WHERE states.name = %s
+        ORDER BY cities.id ASC
+        """, (sys.argv[4])
+    )
+    rows = c.fetchall()
+    print(*(rows), sep=", ")
