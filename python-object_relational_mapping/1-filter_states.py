5b331a156ab3ab53f7862202594fc839f9046eda
force-push-main <10535@holbertonstudents.com>
2025-06-26 12:23:25 +1000

    added code for task 1

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""python script to get states from db using ORM"""
+
+
+if __name__ == '__main__':
+    import MySQLdb
+    import sys
+
+    db = MySQLdb.connect(
+        user=sys.argv[1], password=sys.argv[2], database=sys.argv[3]
+        )
+    c = db.cursor()
+    c.execute("""SELECT states.id, states.name
+              FROM states
+              WHERE states.name LIKE 'N%'
+              ORDER BY states.id ASC""")
+    rows = c.fetchall()
+    for row in rows:
+        print(row)
