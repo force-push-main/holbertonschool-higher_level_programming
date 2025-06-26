1bdd1a72e75251013ba4e667c32f1e9a2395b91b
force-push-main <10535@holbertonstudents.com>
2025-06-26 14:20:45 +1000

    linked by state id

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -14,7 +14,7 @@ if __name__ == '__main__':
     c.execute("""SELECT cities.id, cities.name, states.name
               FROM cities
               INNER JOIN states
-              ON states.id = cities.id
+              ON states.id = cities.state_id
               ORDER BY cities.id ASC"""
               )
     rows = c.fetchall()
