ddfbcd5d840237b35dd48229ce236667cfd1d5fa
force-push-main <10535@holbertonstudents.com>
2025-06-27 16:55:55 +1000

    it worked before

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -16,4 +16,4 @@ if __name__ == "__main__":
     stmt = select(City).join(State, City.state_id == State.id).order_by(City.id)
     results = session.execute(stmt)
     for row in results:
-        print(row.__dict__)
+        print(row)
