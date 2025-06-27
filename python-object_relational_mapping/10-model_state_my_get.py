8092284fc1854be35413a3b1bb7a959c37103ff0
force-push-main <10535@holbertonstudents.com>
2025-06-27 14:52:07 +1000

    added code for task 10

---
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""script that prints state id from search param"""
+
+
+if __name__ == "__main__":
+    import sys
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    from model_state import State
+
+    engine = create_engine(
+        f"mysql://{sys.argv[1]}:{sys.argv[2]}@localhost/{sys.argv[3]}"
+        )
+    Session = sessionmaker(engine)
+    session = Session()
+
+    result = (session.query(State.id)
+              .filter_by(State.name == sys.argv[4])
+              .first())
+    if result is not None:
+        print(result[0])
+    else:
+        print("Nothing")
