1cc29a9c3d45516ff6020f245035cc818332635b
force-push-main <10535@holbertonstudents.com>
2025-06-27 13:56:54 +1000

    only runs if in main

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 """script that lists all state objects from db"""
 
-from model_state import State
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-import sys
+if __name__ == "__main__":
+    from model_state import State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    import sys
 
-url = "mysql+mysqldb://{}:{}@localhost/{}".format(sys.argv[0], sys.argv[1], sys.argv[2])
-engine = create_engine(url)
-Session = sessionmaker(bind=engine)
-session = Session()
-result = session.query(State.id, State.name).order_by(State.id)
+    url = "mysql+mysqldb://{}:{}@localhost/{}".format(sys.argv[0], sys.argv[1], sys.argv[2])
+    engine = create_engine(url)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    result = session.query(State.id, State.name).order_by(State.id)
 
-print(f"{row.id}: {row.name}" for row in result)
+    print(f"{row.id}: {row.name}" for row in result)
