355891759cc66fa0bf55d6cff9fdfbb34dd7bb1f
force-push-main <10535@holbertonstudents.com>
2025-06-27 17:02:34 +1000

    style issues

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -4,7 +4,7 @@
 
 if __name__ == "__main__":
     import sys
-    from sqlalchemy import create_engine, join, select
+    from sqlalchemy import create_engine, select
     from sqlalchemy.orm import Session
     from model_city import City
     from model_state import State
@@ -13,7 +13,9 @@ if __name__ == "__main__":
         f"mysql://{sys.argv[1]}:{sys.argv[2]}@localhost/{sys.argv[3]}"
         )
     session = Session(engine)
-    stmt = select(City, State).join(State, City.state_id == State.id).order_by(City.id)
+    stmt = (select(City, State)
+            .join(State, City.state_id == State.id)
+            .order_by(City.id))
     results = session.execute(stmt)
     for city, state in results:
         print(f"{state.name}: ({city.id}) {city.name}")
