1f4f63cf691f5e0a9584dd8fa78202c89c1f72b3
force-push-main <10535@holbertonstudents.com>
2025-06-27 16:18:29 +1000

    added code for task 13

---
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""script that updateds a record into table"""
+
+
+if __name__ == "__main__":
+    import sys
+    from sqlalchemy import create_engine, delete
+    from sqlalchemy.orm import Session
+    from model_state import State
+
+    engine = create_engine(
+        f"mysql://{sys.argv[1]}:{sys.argv[2]}@localhost/{sys.argv[3]}"
+        )
+    session = Session(engine)
+    stmt = (delete(State)
+            .where(State.name.contains('s')))
+    session.execute(stmt)
+    session.commit()
