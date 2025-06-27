ad6c24eb15df9413178e68da8c6348e5ce52f40c
force-push-main <10535@holbertonstudents.com>
2025-06-27 13:01:55 +1000

    added model state

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+if __name__ == '__main__':
+    from sqlalchemy import create_engine, Column, String, Integer, MetaData
+    from sqlalchemy.ext.declarative import declarative_base
+
+    Base = declarative_base()
+    engine = create_engine("mysql://root:root@localhost/hbtn_0e_6_usa")
+    Base.metadata.create_all(engine)
+
+    class States(Base):
+        __table__name = 'states'
+        id = Column(Integer,
+                    primary_key=True,
+                    autoincrement=True,
+                    nullable=False)
+        name = Column(String(128))
