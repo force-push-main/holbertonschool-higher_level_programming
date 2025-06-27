5c30dfafe856fa6fdca74e9c6bee66027ac5eb63
force-push-main <10535@holbertonstudents.com>
2025-06-27 13:07:49 +1000

    typo in class name

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -2,20 +2,19 @@
 """class with a model for the states stable"""
 
 
-if __name__ == '__main__':
+from sqlalchemy import create_engine, Column, String, Integer, MetaData
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+engine = create_engine("mysql://root:root@localhost/hbtn_0e_6_usa")
+Base.metadata.create_all(engine)
+
+
+class State(Base):
     """State class"""
-    from sqlalchemy import create_engine, Column, String, Integer, MetaData
-    from sqlalchemy.ext.declarative import declarative_base
-
-    Base = declarative_base()
-    engine = create_engine("mysql://root:root@localhost/hbtn_0e_6_usa")
-    Base.metadata.create_all(engine)
-
-    class States(Base):
-        """State class"""
-        __table__name = 'states'
-        id = Column(Integer,
-                    primary_key=True,
-                    autoincrement=True,
-                    nullable=False)
-        name = Column(String(128))
+    __table__name = 'states'
+    id = Column(Integer,
+                primary_key=True,
+                autoincrement=True,
+                nullable=False)
+    name = Column(String(128))
