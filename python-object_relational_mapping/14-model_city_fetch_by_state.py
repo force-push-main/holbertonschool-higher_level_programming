8d94fef36c7a08227e98993b3da6ac27d7d1dd77
force-push-main <10535@holbertonstudents.com>
2025-06-27 16:46:33 +1000

    testing

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -4,7 +4,7 @@
 
 if __name__ == "__main__":
     import sys
-    from sqlalchemy import create_engine
+    from sqlalchemy import create_engine, join, select
     from sqlalchemy.orm import Session
     from model_city import City
     from model_state import State
@@ -13,6 +13,7 @@ if __name__ == "__main__":
         f"mysql://{sys.argv[1]}:{sys.argv[2]}@localhost/{sys.argv[3]}"
         )
     session = Session(engine)
-    results = session.query(City).join(State).all()
+    stmt = select(City).join(State, City.state_id == State.id)
+    results = session.execute(stmt)
     for row in results:
         print(row)
