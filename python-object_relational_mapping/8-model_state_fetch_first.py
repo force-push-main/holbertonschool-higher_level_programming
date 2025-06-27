cd81d41d2fd51ef4dfb5e94f4d3ed426881a9d71
force-push-main <10535@holbertonstudents.com>
2025-06-27 14:25:54 +1000

    added code for task 8

---
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""script that returns first result from db"""
+
+
+if __name__ == "__main__":
+    import sys
+    from model_state import State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+
+    engine = create_engine(
+        f"mysql://{sys.argv[1]}:{sys.argv[2]}@localhost/{sys.argv[3]}"
+        )
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    result = session.query(State.id, State.name).first()
+    for row in result:
+        print(f"{row[0]}: {row[1]}")
