bc0839c2f563ca5993160085355dbf2b54bb7540
force-push-main <10535@holbertonstudents.com>
2025-06-27 14:38:12 +1000

    added code for task 9

---
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""script that returns states with 'c' in name"""
+
+
+if __name__ == "__main__":
+    import sys
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    from model_state import State
+
+    engine = create_engine(
+        f"{sys.argv[1]}:{sys.argv[2]}@localhost/{sys.argv[3]}"
+        )
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    for state in (session.query(State.id, State.name)
+                  .filter(State.name.contains("a"))):
+        print(f"{state[0]}: {state[1]}")
