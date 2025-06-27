e4b23239c17bb203c0bc4eefd869140888e42daf
force-push-main <10535@holbertonstudents.com>
2025-06-27 15:41:32 +1000

    added code for task 12

---
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""script that updateds a record into table"""
+
+
+if __name__ == "__main__":
+    import sys
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    from model_state import State
+
+    engine = create_engine(
+        f"mysql://{sys.argv[1]}:{sys.argv[2]}@localhost/{sys.argv[3]}"
+        )
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    state_to_update = session.query(State).filter(id = 2).first()
+    if state_to_update is not None:
+        state_to_update.name = "New Mexico"
+    session.commit()
