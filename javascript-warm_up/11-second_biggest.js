275bebc6d4af7b1384f75ca2ea00a85f94253222
force-push-main <10535@holbertonstudents.com>
2025-07-16 12:41:43 +1000

    sort by value

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -2,15 +2,21 @@
 
 const { argv } = require('node:process');
 
-const arr = argv.slice(2);
+const raw_arr = argv.slice(2);
+const num_arr = raw_arr.map(Number);
+
+// prettier-ignore
+function compareNumbers (a, b) {
+    return a - b
+}
 
 // prettier-ignore
 function findSecondBiggest (arr) {
   if (arr.length <= 1) {
     return 0;
   }
-  arr.sort();
+  arr.sort(compareNumbers);
   return arr[arr.length - 2];
 }
 
-console.log(findSecondBiggest(arr));
+console.log(findSecondBiggest(num_arr));
