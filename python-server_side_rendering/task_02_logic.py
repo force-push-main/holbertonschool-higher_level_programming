cd7a1719aacb7c75383ab3bc96cf1f131f60e036
force-push-main <10535@holbertonstudents.com>
2025-07-21 11:08:25 +1000

    need to be called items?

--- python-server_side_rendering/task_02_logic.py
+++ python-server_side_rendering/task_02_logic.py
@@ -18,8 +18,8 @@ def contact():
 @app.route('/items')
 def items():
     with open('items.json', 'r') as f:
-        data = json.load(f)
-        return render_template('items.html', data=data)
+        items = json.load(f)
+        return render_template('items.html', items=items)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
