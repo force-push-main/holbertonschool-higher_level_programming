a7407304f0fab560e8c51fe273a593a7a99573a2
force-push-main <10535@holbertonstudents.com>
2025-07-21 10:59:07 +1000

    code for task 3

--- python-server_side_rendering/task_01_jinja.py
+++ python-server_side_rendering/task_01_jinja.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import json
 
 app = Flask(__name__)
 
@@ -13,6 +14,12 @@ def about():
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
+
+@app.route('/items')
+def items():
+    with open('items.json', 'r') as f:
+        data = json.load(f)
+        return render_template('items.html', data=data)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
