f6a21d3a7959af38a5cc56be3247f6dbb92fbd72
force-push-main <10535@holbertonstudents.com>
2025-07-24 14:27:47 +1000

    updated code to provide alternate methods for achieving same functionality

--- javascript-dom_manipulation/2-script.js
+++ javascript-dom_manipulation/2-script.js
@@ -1,5 +1,16 @@
+// const changeColor = () => {
+//   const header = document.querySelector('header');
+//   header.className = 'red';
+// };
+
+// ---- alternate method for updating classname using add event listener -----
+
 const header = document.querySelector('header');
 const redHeader = document.querySelector('#red_header');
 redHeader.addEventListener('click', () => {
   header.className = 'red';
 });
+
+// here to prevent reference error
+
+const changeColor = () => null;
