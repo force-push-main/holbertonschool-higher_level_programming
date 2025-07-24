ee83dd8c46b1d4e36a2fb699d6b881614d9af315
force-push-main <10535@holbertonstudents.com>
2025-07-24 13:59:46 +1000

    added alternate method for toggling color task 3

--- javascript-dom_manipulation/3-script.js
+++ javascript-dom_manipulation/3-script.js
@@ -1,8 +1,29 @@
-const toggleColor = () => {
-  const header = document.querySelector('header');
+// ----- creates onclick function for HTML's native onClick method -----
+
+// const toggleColor = () => {
+//   const header = document.querySelector('header');
+//   if (header.className === 'green') {
+//     header.className = 'red';
+//   } else {
+//     header.className = 'green';
+//   }
+// };
+
+// ----- alternate toggle method using event listener -------
+
+const header = document.querySelector('header');
+const toggleButton = document.querySelector('#toggle_header');
+toggleButton.addEventListener('click', () => {
   if (header.className === 'green') {
     header.className = 'red';
   } else {
     header.className = 'green';
   }
+  console.log('event listener fired');
+});
+
+// ------ here to prevent reference error -------
+
+const toggleColor = () => {
+  return;
 };
